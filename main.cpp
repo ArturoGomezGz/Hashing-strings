6a97b1312eb67af5d7a01646ffc2285c20f939ef
Arturo Gomez <arturogomezgz04@gmail.com>
2025-09-10 10:05:27 -0600

    Refactor hash_string function to simplify hash calculation and remove redundant code

--- main.cpp
+++ main.cpp
@@ -5,22 +5,12 @@ using namespace std;
 
 // hash(s) = (s[0]*p^(n-1) + s[1]*p^(n-2) + ... + s[n-1]*p^0) % m
 
-long long hash_string(string s, int p, long m ){
-
+long long hash_string(string s, int p, long m) {
     long long hash = 0;
     long long power = 1;
-    int n = s.length();
-    // Calcula p^(n-1) para el primer caracter
-    for (int i = 0; i < n - 1; i++) {
+    for (int i = s.length() - 1; i >= 0; i--) {
+        hash = (hash + s[i] * power) % m;
         power = (power * p) % m;
-    }
-    for (int i = 0; i < n; i++) {
-        hash = (hash + (s[i] * power) % m) % m;
-        if (power > 1) {
-            power = (power * modInverse(p, m)) % m;
-        } else {
-            power = 1;
-        }
     }
     return hash;
 }
